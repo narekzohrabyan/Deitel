8938e52abcd093832b9f0d97a43eca87eeba1770
narekzohrabyan <narek.zohrabyan95@gmail.com>
2014-12-02 22:56:33 +0400

    2.26

---
+++ Chapter02/2.26/2.26.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+int main()
+{
+std::cout<<"   * * * * * * * *  "<<std::endl;;
+std::cout<<"    * * * * * * * * "<<std::endl;
+std::cout<<"   * * * * * * * * "<<std::endl;
+std::cout<<"    * * * * * * * * "<<std::endl;
+std::cout<<"   * * * * * * * *  "<<std::endl;
+std::cout<<"     * * * * * * * * "<<std::endl;
+std::cout<<"   * * * * * * * *   "<<std::endl;
+std::cout<<"     * * * * * * * * "<<std::endl;
+std::cout << "* * * * * * * * \n * * * * * * * *\n* * * * * * * * \n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n* * * * * * * * \n * * * * * * * *\n";
+
+
+return 0 ;
+
+}
