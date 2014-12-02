b61e56478d328f85165187733950f6b45025eb31
narekzohrabyan <narek.zohrabyan95@gmail.com>
2014-12-02 12:53:17 +0400

    2.24

---
+++ Chapter02/2.24/2.24.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+
+int main()
+{
+int a;
+std::cout<<"Enter a number ";
+std::cin>>a;
+if (a%2==0)
+	std::cout<< a <<" Pair number";
+else
+	std::cout<< a <<" Odd number";
+return 0;
+}
