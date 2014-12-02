8a24f21a196b99a556d388407f18b454575c15ae
narekzohrabyan <narek.zohrabyan95@gmail.com>
2014-12-02 12:08:26 +0400

    C++

---
+++ Chapter02/2.16/2.16.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+int main()
+{
+   int i, j;
+
+ std::cout << "Enter numbers  ";
+ std ::cin >> i >> j;
+ std ::cout << "sum = " << i + j <<"\n" ;
+ std ::cout    << "product = " << i * j <<'\n';
+ std ::cout     << "difference = " << i - j <<"\n"  ;
+ std::cout     << "quotient = " << i / j ;
+
+   return 0;
+}
